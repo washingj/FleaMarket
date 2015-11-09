af102a357e2f3d183b22e553c81f5770cbe365da
washingj <skynesssnail@gmail.com>
2015-11-09 13:15:20 -0500

    Fragment
    Fragment

---
+++ app/src/main/java/com/example/fragmenttabhostviewpager/Fragment2.java
@@ -0,0 +1,26 @@
+package com.example.fragmenttabhostviewpager;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Fragment2 extends Fragment {
+	TextView msg;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment2, container,
+				false);
+		msg = (TextView) rootView.findViewById(R.id.msg);
+
+		return rootView;
+	}
+
+	public void setMsg(String text) {
+		msg.setText(text);
+	}
+}
